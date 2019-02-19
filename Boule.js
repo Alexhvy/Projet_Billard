d8501071856025d1fdfa335f8d9c39d715f62bee
Alexhvy <alexandre.hervy@edu.ece.fr>
2019-02-19 16:16:54 +0100

    Ajout de la classe boule

---
+++ Boule.js
@@ -0,0 +1,18 @@
+function Boule (x,y){
+  this.pos = createVector(x,y);
+  this.acc = createVector(random(0,5),0);
+  this.vel = createVector(random(5,40),0);
+
+  this.update = function (){
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0.09);
+
+  }
+
+  this.draw = function (){
+
+    circle(this.pos.x,this.pos.y,15);
+
+  }
