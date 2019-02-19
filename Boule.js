e6ccb8fc69175da1f6a8fc7f379b5e138861e0b5
Alexhvy <alexandre.hervy@edu.ece.fr>
2019-02-19 16:48:43 +0100

    Update

--- Boule.js
+++ Boule.js
@@ -2,6 +2,7 @@ function Boule (x,y){
   this.pos = createVector(x,y);
   this.acc = createVector(random(0,5),0);
   this.vel = createVector(random(5,40),0);
+  this.rayon = 15;
 
   this.update = function (){
 
@@ -9,10 +10,13 @@ function Boule (x,y){
     this.pos.add(this.vel);
     this.acc.mult(0.09);
 
-  }
-
-  this.draw = function (){
-
-    circle(this.pos.x,this.pos.y,15);
 
   }
+
+
+  this.show = function (){
+
+    circle(this.pos.x,this.pos.y,this.rayon);
+
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,10 +1,24 @@
+boules = [];
+
 function setup() {
   // put setup code here
+  createCanvas(1600,800);
+  fill(255);
+  for (let i=0;i<3;i++){
+    boules.push(new Boule(random(300),random(300)));
+  }
+  frameRate(10);
 
 }
 
 
 function draw() {
   // put drawing code here
+background(51);
+for (let i=0;i<boules.length;i++){
+    boules[i].update();
+    boules[i].show();
+}
+
 
 }
