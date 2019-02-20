35f7277b3eea1eff05e32287c25dca6d47d6d270
Alexhvy <alexandre.hervy@edu.ece.fr>
2019-02-20 15:25:04 +0100

    Première gestion des collisions entre les boules ! :)

--- Boule.js
+++ Boule.js
@@ -1,15 +1,16 @@
-function Boule (x,y){
+function Boule (x,y,acc){
   this.pos = createVector(x,y);
-  this.acc = createVector(random(0,5),0);
-  this.vel = createVector(random(5,40),0);
+  this.acc = createVector(acc,0);
+  this.vel = createVector(0,0);
   this.rayon = 15;
 
   this.update = function (){
 
+
     this.vel.add(this.acc);
     this.pos.add(this.vel);
-    this.acc.mult(0.09);
-
+    this.acc.mult(0);
+    this.vel.mult(0.96);
 
   }
 
@@ -19,4 +20,16 @@ function Boule (x,y){
     circle(this.pos.x,this.pos.y,this.rayon);
 
   }
+
+  this.gestionCollision = function (other)
+  {
+    let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
+      if(d < this.rayon + other.rayon){
+        return true;
+      }
+      else {
+        return false;
+      }
+  }
+
 }
--- sketch.js
+++ sketch.js
@@ -4,9 +4,11 @@ function setup() {
   // put setup code here
   createCanvas(1600,800);
   fill(255);
-  for (let i=0;i<3;i++){
-    boules.push(new Boule(random(300),random(300)));
-  }
+  boules.push(new Boule(50,300,15));
+  boules.push(new Boule(400,300,-45));
+  boules.push(new Boule(427,285,-5));
+  boules.push(new Boule(427,315,-3));
+
   frameRate(10);
 
 }
@@ -15,10 +17,17 @@ function setup() {
 function draw() {
   // put drawing code here
 background(51);
-for (let i=0;i<boules.length;i++){
-    boules[i].update();
-    boules[i].show();
-}
-
-
+for (let ball of boules){
+    ball.update();
+    ball.show();
+    let contact = false;
+    for(let otherBall of boules){
+      if(ball != otherBall && ball.gestionCollision(otherBall)){
+          contact = true;
+      }
+    }
+    if (contact){
+      ball.vel.mult(-1);
+    }
+  }
 }
